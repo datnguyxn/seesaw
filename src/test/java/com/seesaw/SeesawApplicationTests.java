3abbbb48104eaa2f2ec6f64946d921501e3f7245
torondatto <nguyentrongdat1108@gmail.com>
2023-06-25 17:06:24 +0700

    add project

---
+++ src/test/java/com/seesaw/SeesawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seesaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeesawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
