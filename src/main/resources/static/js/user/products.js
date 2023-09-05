30d998beb99e6805762de8f424153d63a5ec10be
XID <86798664+Alexid-ID@users.noreply.github.com>
2023-09-05 22:17:34 +0700

    fiter product, fix order controller

--- src/main/resources/static/js/user/products.js
+++ src/main/resources/static/js/user/products.js
@@ -1,20 +1,37 @@
 // js to get all products data
 const products = $('.products');
+const type = $('#type').val();
 
-//ajax request to get all products
 $(document).ready(function () {
-    $.ajax({
-        url: "/products/list",
-        type: "GET",
-        dataType: "json",
-        contentType: "application/json",
-        success: function (data) {
-            console.log(data);
-        },
-        error: function (error) {
-            console.log("Error");
-        }
+    renderCategoriesFilter();
+
+    $('.product_card').on('click', function (e) {
+        window.location.href = `/product-detail?id=${$(this).data('id')}`;
     })
 });
+
+function renderCategoriesFilter() {
+    let html = '';
+    let filterCategories = $('.filter-categories');
+    if(type === 'all') {
+        $('.choice.all').addClass('active');
+    } else {
+        $('.choice.all').removeClass('active');
+    }
+
+    $.ajax({
+        url: '/api/categories/list',
+        type: 'GET',
+        success: function (data) {
+            data.content.forEach(function (category) {
+                html += `<a class="choice ${type=== category.id ? "active":"" } text-decoration-none text-dark" data-id="${category.id}" href="/products?type=${category.id}">${category.name}</a>`
+            })
+            filterCategories.append(html);
+        },
+        error: function (error) {
+            console.log(error.responseText);
+        }
+    })
+}
 
 
